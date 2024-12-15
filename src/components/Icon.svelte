fc63fa1e0c6557b5c9b7e6f1c051be3ef466d2a4
TomasMusi <tomas.musilmt@gmail.com>
2024-12-15 14:12:34 +0100

    Added Icons

---
+++ src/components/Icon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { string } from 'zod';
+    //                  kvůli tomu, že class existuje v Sveltkitu tak to musíme přejmenovat.
+    // class: cls = ''; Jakože je to defaultně prázdný
+    const { name, class: cls = '' }: { name: string; class?: string } = $props();
+</script>
+
+<!-- BI jako Boostrap icons-->
+<i class="bi {name} {cls}"></i>
