af728e722679e9ddbda45a8dfd7311dfd31093e9
TomasMusi <tomas.musilmt@gmail.com>
2024-12-15 10:31:35 +0100

    Fixed Positions of Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+    // zde musí být import na ../app.css aby tailwind fungoval.
     import '../app.css';
     let { children } = $props();
 </script>
 
 <main class="bg-background text-text h-full min-h-screen w-full overflow-x-hidden lg:text-lg">
-    <nav class="bg-secondary flex flex-row gap-4 px-2 md:px-8">
-        <h1 class="text-primary text-2xl lg:text-4xl">Forum</h1>
+    <nav class="bg-secondary grid-cols-nav grid gap-4 px-2 py-4 md:px-8">
+        <h1 class="text-primary text-3xl font-bold lg:text-5xl">Forum</h1>
+        <div class="flex gap-2">item</div>
+        <div class="flex justify-end gap-2">Login</div>
     </nav>
-
     {@render children()}
 </main>
