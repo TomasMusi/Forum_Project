77dd64cd91f13ad69a2a8ac3f6e5befd14467be2
TomasMusi <tomas.musilmt@gmail.com>
2024-12-16 19:18:47 +0100

    Added Card Icon to Components

---
+++ src/components/Card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { twMerge } from 'tailwind-merge';
+    import { MergeQueryBuilder } from 'kysely';
+    import type { Snippet } from 'svelte';
+
+    const { children, class: cls = '' }: { children: Snippet; class: string } = $props();
+</script>
+
+<!-- Co dělá twMerge ?-->
+<!-- tw.Merge("text-xl font-bold text-red-500" , "text-lg") výstup bude font-bold text-red-500 TEXT-LG -->
+<!-- Jak můžeme vidět tak to vymění  -->
+<!-- Ale když tam zadáme něco co tam není tak to tam prostě přidá ( Nemá to s čím vyměnit)-->
+<div class={twMerge('flex flex-col items-center justify-center rounded-md border-2 border-primary bg-accent p-4', cls)}>
+    {@render children()}
+</div>
