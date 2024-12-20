1fd10022583d9bf69fc9187d2f2b9771a4fd978c
TomasMusi <tomas.musilmt@gmail.com>
2024-12-20 19:00:05 +0100

    Register page Done

--- src/components/Form/Entry.svelte
+++ src/components/Form/Entry.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import type { Snippet } from 'svelte';
 
-    const { children, id = '', label, error = undefined }: { children: Snippet; id: string; label: string; error?: string } = $props();
+    const { children, id = '', label, error = undefined }: { children: Snippet; id?: string; label: string; error?: string } = $props();
 </script>
 
-<div class="flex flex-col gap-2">
-    <label for={id}>{label}</label>
+<div class="flex flex-col">
+    <label class="mb-2 text-lg font-bold lg:text-xl" for={id}>{label}</label>
     {@render children()}
-    {#if error}
-        <span>Error text</span>
-    {/if}
+    <!-- class:invisible udělá to, že to schová a error ?? '...' jestliže je error undefined tak tam budou 3 tečky jinak se to vypíše a v tomto případě to děláme pro to odsazení. Aby kdyby se zobrazil error aby to neposkočilo. -->
+    <span class:invisible={!error} class="text-sm text-red-500">{error ?? '...'}</span>
 </div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
     import Card from '$/components/Card.svelte';
+    import Button from '$/components/Form/Button.svelte';
+    import Entry from '$/components/Form/Entry.svelte';
     import Input from '$/components/Form/Input.svelte';
 </script>
 
-<Card class="">
-    <h1 class="text-2xl font-bold lg:text-4xl">Register</h1>
-    <Input placeholder="Jmeno" />
+<Card class="m-auto grid min-w-[50%] grid-cols-2 gap-8">
+    <h1 class="font-poppins col-span-2 mx-auto mb-4 w-max border-b-2 border-b-black text-2xl font-bold lg:text-4xl">Register</h1>
+    <Entry id="username" label="username" error="IDK">
+        <Input id="username" placeholder="Jmeno" />
+    </Entry>
+    <Entry id="email" label="email" error="IDK">
+        <Input id="email" placeholder="email" type="email" />
+    </Entry>
+    <Entry id="password" label="password" error="IDK">
+        <Input id="password" placeholder="password" type="password" />
+    </Entry>
+    <Entry id="password2" label="password" error="IDK">
+        <Input id="password2" placeholder="password" type="password" />
+    </Entry>
+    <Button class="col-span-2 mx-auto">aa</Button>
 </Card>
