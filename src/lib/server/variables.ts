e643cf02687c46f26db6683e574cc5b385a6cfd0
TomasMusi <tomas.musilmt@gmail.com>
2024-12-22 13:15:23 +0100

    Added Database

--- src/lib/server/variables.ts
+++ src/lib/server/variables.ts
@@ -1,7 +1,9 @@
 // tady si vytvoříme proměnné, které chceme používat na serveru.
 // například conn pro připojení k databázi, nebo proměnnou pro práci třeba s cookies
 
-import { DATABASE_IP, DATABASE_PORT, DATABASE_USER } from "$env/static/private";
+import type { DB } from "$/types/database";
+import { DATABASE_IP, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USER } from "$env/static/private";
+import { Kysely } from "kysely";
 import { MysqlDialect } from "kysely";
 import { createPool } from 'mysql2';
 
@@ -10,13 +12,31 @@ import { createPool } from 'mysql2';
 
 
 const dialect = new MysqlDialect({
+    //@ts-ignore 
+    // (Zde se objeví chyba, ale normálně to funguje akorát to hází typovou chybu, kvůli tomu jak ty package jsou vyvinuty.)
     pool: createPool({
         host: DATABASE_IP,
         port: parseInt(DATABASE_PORT), // semka musíme dát parseInt(), protože v .env souboru jsou všechny věci ve stringu. A port musí být number.
         user: DATABASE_USER,
+        password: DATABASE_PASSWORD,
+        database: DATABASE_NAME,
     })
-})
+});
+
+
+
+
+// Existují dva typy připojování k databázi basic, který zabere více času ,,Hej chci data, připojím se získám je odpojím se" nebo tu existuje způsob Pool
+
 // Jak funguje pool? udělá dopředu 10 připojení na databázi, a když mi chceme udělat nějaký SQL příkaz
 // tak on nemusí udělat to, že musí teď říct: ,,Hej já tady mám udaje tak se musím připojit k databázi" a to chvíli trvá. On v podstatě má už nějaké připojení dopředu udělané.
 // Například my řekneme: ,,Hej já chci udělat tento SQL příkaz, on nám řekne jo tady máš připojení" zde píšeme připojení.
 // On nám vytváří připojení dopředu a potom nám vrací připojení do toho poolu.
+
+
+
+
+//   Kyselymu musíme dát typy z DB
+export const conn = new Kysely<DB>({
+    dialect // zde dáme proměnnou z vrchu ve které jsme nastavovali, připojení na databázi.
+})
---
+++ src/routes/api/+server.ts
@@ -0,0 +1,148 @@
+// Vše co dáme sem se jen a pouze vykoná na serveru. 
+// Můžeme tu používat GET,POST 
+
+import type { RequestHandler } from "@sveltejs/kit";
+
+// export const GET = // sem můžeme vložit funkci, která se vykoná v moment, kdy zašlu GET požadavek na tento API server.
+// export const POST = // to stejné zde.
+
+export const POST = async function ({ request }) {
+    const json = await request.json()
+    console.log(json) // důvod proč to furt řve je ten, že musíme uživateli vrátit nějaký reponse.
+    return new Response("Ahoj");
+} satisfies RequestHandler
+
+
+// nyní když se pošle POST požadavek na server tak se vypíše ahoj
+
+// posílám post request na /api s nějakým json tělem.
+
+
+//fetch("/api", {
+//    method: "post",
+//    body: '{"username": "tomáš", "password": "heslo"}'
+//})
+
+
+// kdybychom se koukli do network tabu, tak vidíme, že nám to vrátilo "Ahoj"
+// a jen a pouze na serverové stráně vídíme to dělo jenž se poslalo.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// kdybychom chtěli udělat funkční server, tak bychom museli udělat složkovou strukturu takto. Musíme to routovat podle složek. Tudíž nám vznikne hromada souborů.
+
+/*
+tree
+.
+├── article
+│   ├── ban
+│   │   └── +server.ts
+│   ├── comments
+│   │   ├── delete
+│   │   │   └── +server.ts
+│   │   ├── get
+│   │   │   └── +server.ts
+│   │   ├── load
+│   │   │   └── +server.ts
+│   │   └── send
+│   │       └── +server.ts
+│   ├── create
+│   │   └── +server.ts
+│   ├── delete
+│   │   └── +server.ts
+│   ├── edit
+│   │   └── +server.ts
+│   ├── get
+│   │   └── +server.ts
+│   ├── history
+│   │   ├── change
+│   │   │   └── +server.ts
+│   │   ├── remove
+│   │   │   └── +server.ts
+│   │   └── +server.ts
+│   ├── check
+│   │   └── +server.ts
+│   ├── rating
+│   │   ├── get
+│   │   │   └── +server.ts
+│   │   └── send
+│   │       └── +server.ts
+│   ├── search
+│   │   └── +server.ts
+│   └── unban
+│       └── +server.ts
+├── bans
+│   └── +server.ts
+├── categories
+│   ├── get
+│   │   └── +server.ts
+│   └── raw
+│       └── +server.ts
+├── category
+│   ├── create
+│   │   └── +server.ts
+│   ├── delete
+│   │   └── +server.ts
+│   ├── edit
+│   │   └── +server.ts
+│   ├── get
+│   │   └── +server.ts
+│   ├── getPath
+│   │   └── +server.ts
+│   └── check
+│       └── +server.ts
+├── file
+│   └── +server.ts
+├── lockdown
+│   ├── get
+│   │   └── +server.ts
+│   └── toggle
+│       └── +server.ts
+├── logged
+│   └── +server.ts
+├── login
+│   └── +server.ts
+├── logout
+│   └── +server.ts
+├── regex
+│   ├── delete
+│   │   └── +server.ts
+│   ├── edit
+│   │   └── +server.ts
+│   ├── get
+│   │   └── +server.ts
+│   └── new
+│       └── +server.ts
+└── user
+    └── getPermissions
+        └── +server.ts
+
+46 directories, 37 files
+*/
+
+
+
+// Naštěstí MegaMozek Patrik udělal package, který tohle za nás vyřeší.
+// SvelekitApi zamezuje tomu, abychom měli strašně moc složek, a má vlastní routovací systém. 
+// Nebude používat router co používá sveltekit na základě složek, ale bude dělat routy našim způsobem.
+// Například můžeme si nadefinovat /API/out/register, My si nějakým způsobem nadefinujeme a pak když půjdeme na tuhle cestu tak se nám spustí nějaká funkce.
+
+// 1. Musíme si udělat kontext, kontext je něco co se předává mezi požadavkama. Kontext dokáže ukládat data a posílat je dál, mezi funkcema.
