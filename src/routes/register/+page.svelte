96aa873bfab8124fad7dbeb888504c0953a1fff1
TomasMusi <tomas.musilmt@gmail.com>
2024-12-26 20:17:48 +0100

    Added Register Page Functionality.

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,16 +3,19 @@
     import Button from '$/components/Form/Button.svelte';
     import Entry from '$/components/Form/Entry.svelte';
     import Input from '$/components/Form/Input.svelte';
+    import { API } from '$/lib/api';
+    import { GroupByItemNode } from 'kysely';
 
     // avšak my chceme říct, že ten error může být i string, akorát je tam undefined? Dáme to přes typy :D
     // existuje typ Record, typicky když chceš mít nějaký object, který bude mít v levo string a potom nějakou value tak se to jmenuje Record.
     // A my mu řekneme, klíče v tomto případě (username, email, password, password2), budou typu string, a druhá hodnota je to co je ve vnitř jich takže v tomto případě (value, error)
     let data = $state<
         Record<
-            string,
+            'username' | 'email' | 'password' | 'password2',
             {
                 value: string;
                 error: string | undefined;
+                // error?: string, toto je to stejné jak nahoře.
             }
         >
     >({
@@ -33,21 +36,76 @@
             error: undefined
         }
     });
+
+    const register = async () => {
+        // my bychom chtěli aby když už tam něco vyplníme tak aby tam nebyl error, hmmm ale jak to udělat? abychom neměli miliardu else statmentů? přes object ne přeciii :D
+
+        // Object.values(data) získá od každého value: "" a error:
+        //Foreach, použijeme protože je tam více elementů.
+        Object.values(data).forEach((item) => (item.error = undefined));
+
+        if (!data.username.value) {
+            data.username.error = 'Please enter Username';
+        }
+        if (!data.email.value) {
+            data.email.error = 'Please enter Email!';
+        }
+        if (!data.password.value) {
+            data.password.error = 'Please Enter A Password!';
+        }
+        if (!data.password2.value) {
+            data.password2.error = 'Please Enter A Password Again!';
+        }
+
+        if (data.password.value != data.password2.value) {
+            data.password.error = "Passwords doesn't match!";
+            data.password2.error = "Passwords doesn't match!";
+        }
+
+        // jestliže se některý item nerovná undefined
+        if (Object.values(data).some((item) => item.error !== undefined)) {
+            return;
+        }
+
+        // console.log(Object.entries(data));
+
+        // Object entries dělá to, že nám to převede na array, který vypadá následovně:
+        // vrátí nám to array, kde v levo je název a v pravo je object
+        // tudíž zde na levo username v pravo object username.
+        // Array(4)0:
+        // (0) ['username', Proxy(Object)]
+        // (1) ['email', Proxy(Object)]
+        // (2) ['password', Proxy(Object)]
+        // (3) ['password2', Proxy(Object)]
+
+        // no, ale asi ideálně bychom chtělo aby vedlo toho byla ta hodnota ne? uděláme to přes .map():D
+
+        // console.log(Object.entries(data).map(([key, value]) => [key, value.value]));
+        // akorát by to ještě chtělo aby se to neposílalo v array že? Na to Existuje Object.FromEntries()! :D
+        // .map() udělá to, že nám to vyfiltruje a napíše nám to vedle našeho klíče (username, email, password,password2) vypíše tu value.
+
+        // Object.fromEntries nám z toho netries udělá object znovu.
+        const body = Object.fromEntries(Object.entries(data).map(([key, value]) => [key, value.value]));
+
+        // akorát nyní je tu problém, ono to nerozeznává náš, email, password, proto nahoře musíme přesně říct, že to bude username string,email,string,password string,password2 string
+        const response = await API.auth.register(body);
+    };
 </script>
 
 <Card class="m-auto grid min-w-[50%] grid-cols-2 gap-8">
     <h1 class="font-poppins col-span-2 mx-auto mb-4 w-max border-b-2 border-b-black text-2xl font-bold lg:text-4xl">Register</h1>
-    <Entry id="username" label="Username">
-        <Input id="username" placeholder="Username" />
+    <Entry id="username" label="Username" error={data.username.error}>
+        <!-- důvod proč nepoužijeme u toho error, bind, je ten že bind používáme tehdy, když chceme posílat a získavat data zpátky, což zde nechceme. -->
+        <Input id="username" placeholder="Username" bind:value={data.username.value} />
     </Entry>
-    <Entry id="email" label="Email">
-        <Input id="email" placeholder="email" type="email" />
+    <Entry id="email" label="Email" error={data.email.error}>
+        <Input id="email" placeholder="email" type="email" bind:value={data.email.value} />
     </Entry>
-    <Entry id="password" label="Password">
-        <Input id="password" placeholder="password" type="password" />
+    <Entry id="password" label="Password" error={data.password.error}>
+        <Input id="password" placeholder="password" type="password" bind:value={data.password.value} />
     </Entry>
-    <Entry id="password2" label="Password (Again)">
-        <Input id="password2" placeholder="password" type="password" />
+    <Entry id="password2" label="Password (Again)" error={data.password2.error}>
+        <Input id="password2" placeholder="password" type="password" bind:value={data.password2.value} />
     </Entry>
-    <Button class="col-span-2 mx-auto">Register</Button>
+    <Button class="col-span-2 mx-auto" onclick={register}>Register</Button>
 </Card>
